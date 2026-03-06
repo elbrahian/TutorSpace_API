7e210ce75eaea184a5d6fe66b9d028441fb54eb2
elbrahian <brahianrueda123@gmail.com>
2026-03-05 22:09:53 -0500

    add secutiryconfig, jwt, emailvalidator config

---
+++ src/main/java/com/uco/tutorspace_api/Utils/EmailValidator.java
@@ -0,0 +1,9 @@
+package com.uco.tutorspace_api.Utils;
+
+public class EmailValidator {
+    private static final String DOMINIO_INSTITUCIONAL = "@uco.net.co";
+
+    public boolean esCorreoInstitucional(String email) {
+        return email != null && email.toLowerCase().endsWith(DOMINIO_INSTITUCIONAL);
+    }
+}
---
+++ src/main/java/com/uco/tutorspace_api/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.uco.tutorspace_api.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtAuthFilter jwtAuthFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/ws/**").permitAll()      // WebSocket
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/tutor/**").hasRole("TUTOR")
+                        .requestMatchers("/estudiante/**").hasRole("ESTUDIANTE")
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
