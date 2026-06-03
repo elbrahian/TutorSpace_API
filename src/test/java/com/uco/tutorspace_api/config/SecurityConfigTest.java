0be49a43331df8fcec210ded167f37379562d978
elbrahian <brahianrueda123@gmail.com>
2026-06-02 22:27:59 -0500

    configuracion tests no usen la db, solo una temporal en memoria

--- src/test/java/com/uco/tutorspace_api/config/SecurityConfigTest.java
+++ src/test/java/com/uco/tutorspace_api/config/SecurityConfigTest.java
@@ -4,18 +4,18 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@WebAppConfiguration
 @ActiveProfiles("test")
 @DisplayName("Pruebas de Seguridad - SecurityConfig")
 class SecurityConfigTest {
