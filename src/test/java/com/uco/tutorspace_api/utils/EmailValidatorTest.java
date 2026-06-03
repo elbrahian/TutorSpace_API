a7a015cf93f4d10675a38561ea4a20eb8a2c7d37
Maria Jose <majorr1017@gmail.com>
2026-06-02 20:04:44 -0500

    Corrección de la deuda técnica: SonarQube

--- src/test/java/com/uco/tutorspace_api/utils/EmailValidatorTest.java
+++ src/test/java/com/uco/tutorspace_api/utils/EmailValidatorTest.java
@@ -1,6 +1,5 @@
-package com.uco.tutorspace_api.Utils;
+package com.uco.tutorspace_api.utils;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
