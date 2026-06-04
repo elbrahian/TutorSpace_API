439522fd0f2ba4cbe52c2a719541711be1b68a5f
JDMQXD <juandamartinez04@gmail.com>
2026-06-04 10:49:29 -0500

    Se agrega la funcionalidad de filtros para las consultas de auditoria, se usa especification ya que permite hacer una consulta mas limpia y no tan grande

--- src/main/java/com/uco/tutorspace_api/service/AuditoriaService.java
+++ src/main/java/com/uco/tutorspace_api/service/AuditoriaService.java
@@ -1,13 +1,18 @@
 package com.uco.tutorspace_api.service;
 
+import com.uco.tutorspace_api.domain.HistorialSesion;
 import com.uco.tutorspace_api.domain.dto.AuditoriaSesionResponse;
+import com.uco.tutorspace_api.domain.enums.EstadoSesion;
 import com.uco.tutorspace_api.repositories.HistorialSesionRepository;
+import com.uco.tutorspace_api.repositories.HistorialSesionSpecification;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 
+// Se crea el service de auditoria donde se puede consultar todos los campos necesarios
 
 @Service
 @RequiredArgsConstructor
@@ -16,10 +21,28 @@ public class AuditoriaService {
     private final HistorialSesionRepository historialRepository;
 
     public Page<AuditoriaSesionResponse> obtenerAuditoria(
+            EstadoSesion estadoNuevo,
+            EstadoSesion estadoAnterior,
+            String tutor,
+            String estudiante,
             Pageable pageable
     ) {
 
-        return historialRepository.findAll(pageable)
+        Specification<HistorialSesion> spec =
+                Specification.where(
+                                HistorialSesionSpecification.estadoNuevo(estadoNuevo)
+                        )
+                        .and(
+                                HistorialSesionSpecification.estadoAnterior(estadoAnterior)
+                        )
+                        .and(
+                                HistorialSesionSpecification.tutor(tutor)
+                        )
+                        .and(
+                                HistorialSesionSpecification.estudiante(estudiante)
+                        );
+
+        return historialRepository.findAll(spec, pageable)
                 .map(h -> new AuditoriaSesionResponse(
                         h.getSesion().getId(),
                         h.getSesion().getTutor().getNombre(),
