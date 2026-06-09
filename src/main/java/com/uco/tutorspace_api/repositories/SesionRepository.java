060f4edeb21b30e2cc4ad1414ca75c9f1fbfbf4f
Juanjo5e <juanjoc2703@gmail.com>
2026-06-09 18:22:11 -0500

    Se corrigen los impor de pageable y  EstadoSesion en SesionRepository

--- src/main/java/com/uco/tutorspace_api/repositories/SesionRepository.java
+++ src/main/java/com/uco/tutorspace_api/repositories/SesionRepository.java
@@ -1,9 +1,11 @@
 package com.uco.tutorspace_api.repositories;
 
 import com.uco.tutorspace_api.domain.Sesion;
+import com.uco.tutorspace_api.domain.enums.EstadoSesion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
