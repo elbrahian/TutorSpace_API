1444c40e53a7e2aebfc7834078f41f328da5ad43
juan diego guzman  chalarca <juandiguzman06@gmail.com>
2026-06-08 23:38:40 -0500

    test(mnt-05): re-agregar mock de ChatService en SesionServiceTest

--- src/test/java/com/uco/tutorspace_api/service/SesionServiceTest.java
+++ src/test/java/com/uco/tutorspace_api/service/SesionServiceTest.java
@@ -39,6 +39,7 @@ class SesionServiceTest {
     @Mock private DisponibilidadService disponibilidadService;
     @Mock private HistorialSesionService historialSesionService;
     @Mock private NotificacionService notificacionService;
+    @Mock private ChatService chatService; // MNT-05
     @Mock private CalificacionSesionRepository calificacionSesionRepository;
 
     @InjectMocks
